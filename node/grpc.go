e9edce6320fbab1c23491f8ee4ccf7bfbd9e4bee
baoyangchai@gmail.com <baoyangchai@gmail.com>
2022-07-29 10:39:40 +0800

    add some comment for develop new rpc method and change variable name

--- node/grpc.go
+++ node/grpc.go
@@ -13,44 +13,67 @@ import (
 	"google.golang.org/grpc"
 )
 
-type BalanceService struct {
+type ApiService struct {
 	//*eth.Ethereum
 	//*eth.MinerAPI
 	//*eth.AdminAPI
+	//*ethapi.NetAPI
 	*ethapi.EthereumAPI
 	*ethapi.BlockChainAPI
 	*ethapi.TransactionAPI
 	*ethapi.TxPoolAPI
 	*ethapi.DebugAPI
+	*ethapi.EthereumAccountAPI
+	*ethapi.PersonalAccountAPI
 	protoeth.UnimplementedBalanceServer
 }
 
-func NewService(bkd ethapi.Backend) *BalanceService {
+func NewService(bkd ethapi.Backend) *ApiService {
 
-	//cfg := n.Config()
-	//bkd := &eth.EthAPIBackend{}
-	//cfg.ExtRPCEnabled(), cfg.AllowUnprotectedTxs, e
 	lock := new(ethapi.AddrLocker)
-	return &BalanceService{
-		EthereumAPI:    ethapi.NewEthereumAPI(bkd),
-		BlockChainAPI:  ethapi.NewBlockChainAPI(bkd),
-		TransactionAPI: ethapi.NewTransactionAPI(bkd, lock),
-		TxPoolAPI:      ethapi.NewTxPoolAPI(bkd),
-		DebugAPI:       ethapi.NewDebugAPI(bkd),
+	am := bkd.AccountManager()
+
+	return &ApiService{
+		EthereumAPI:        ethapi.NewEthereumAPI(bkd),
+		BlockChainAPI:      ethapi.NewBlockChainAPI(bkd),
+		TransactionAPI:     ethapi.NewTransactionAPI(bkd, lock),
+		TxPoolAPI:          ethapi.NewTxPoolAPI(bkd),
+		DebugAPI:           ethapi.NewDebugAPI(bkd),
+		EthereumAccountAPI: ethapi.NewEthereumAccountAPI(am),
+		PersonalAccountAPI: ethapi.NewPersonalAccountAPI(bkd, lock),
 	}
 }
 
-func (blc *BalanceService) GetBlockNumber(ctx context.Context, args *protoeth.GetBlockNumberReq) (*protoeth.GetBlockNumberResp, error) {
-	hight := blc.BlockChainAPI.BlockNumber()
+func (s *ApiService) GetBlockNumber(ctx context.Context, args *protoeth.GetBlockNumberReq) (*protoeth.GetBlockNumberResp, error) {
+	hight := s.BlockChainAPI.BlockNumber()
 	return &protoeth.GetBlockNumberResp{
 		Number: uint64(hight),
 	}, nil
 }
 
-func (blc *BalanceService) GetBalance(ctx context.Context, args *protoeth.GetBalanceReq) (*protoeth.GetBalanceResp, error) {
+//howto method is an template for add new method for ApiService
+//Before rewrite this method, please define the args , reply
+//and the rpc method in the file grpc/proto/eth.pro. After that ,
+//run `protoc --go_grpc_out=. --go_out=$PWD \*.proto` in the directory grpc/proto.For `protoc`  please read `https://grpc.io/docs/languages/go/quickstart/`
+// func (s *ApiService) howTo(ctx context.Context, args interface{}) (reply interface{}, err error) {
+// 	//we need change the args of `GetTransactionByHash` with args
+// 	trx, err := s.TransactionAPI.GetTransactionByHash(ctx, common.Hash{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	//translate the result to the format of reply
+// 	bdata, err := json.Marshal(trx)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	err = json.Unmarshal(bdata, reply)
+// 	return
+// }
+
+func (s *ApiService) GetBalance(ctx context.Context, args *protoeth.GetBalanceReq) (*protoeth.GetBalanceResp, error) {
 	addr := common.BytesToAddress([]byte(args.Address))
 
-	amount, err := blc.BlockChainAPI.GetBalance(ctx, addr, rpc.BlockNumberOrHashWithNumber(rpc.LatestBlockNumber))
+	amount, err := s.BlockChainAPI.GetBalance(ctx, addr, rpc.BlockNumberOrHashWithNumber(rpc.LatestBlockNumber))
 	if err != nil {
 		return nil, err
 	}
