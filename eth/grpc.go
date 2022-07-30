2e00d96d588c2e46959399510b14410b9d04d847
gopherchai <baoyangchai@gmail.com>
2022-07-30 16:25:03 +0800

    adapt grpcservice

--- eth/grpc.go
+++ eth/grpc.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/eth/downloader"
+	"github.com/ethereum/go-ethereum/eth/filters"
 	"github.com/ethereum/go-ethereum/grpc/proto/protoeth"
 	"github.com/ethereum/go-ethereum/internal/ethapi"
 	"github.com/ethereum/go-ethereum/node"
@@ -11,7 +13,15 @@ import (
 )
 
 type GrpcService struct {
-	*Ethereum
+	//*Ethereum
+	ethEthereumAPI *EthereumAPI
+	ethMinerAPI    *MinerAPI
+	*downloader.DownloaderAPI
+	*filters.FilterAPI
+	ethAdminAPI *AdminAPI
+	ethDebugAPI *DebugAPI
+	*ethapi.NetAPI
+
 	*ethapi.EthereumAPI
 	*ethapi.BlockChainAPI
 	*ethapi.TransactionAPI
@@ -24,19 +34,44 @@ type GrpcService struct {
 
 func NewGrpcService(node *node.Node, e *Ethereum, bkd ethapi.Backend) *GrpcService {
 
-	lock := new(ethapi.AddrLocker)
-	am := bkd.AccountManager()
-
-	return &GrpcService{
-		Ethereum:           e,
-		EthereumAPI:        ethapi.NewEthereumAPI(bkd),
-		BlockChainAPI:      ethapi.NewBlockChainAPI(bkd),
-		TransactionAPI:     ethapi.NewTransactionAPI(bkd, lock),
-		TxPoolAPI:          ethapi.NewTxPoolAPI(bkd),
-		DebugAPI:           ethapi.NewDebugAPI(bkd),
-		EthereumAccountAPI: ethapi.NewEthereumAccountAPI(am),
-		PersonalAccountAPI: ethapi.NewPersonalAccountAPI(bkd, lock),
+	//
+	apis := e.APIs()
+	var s = &GrpcService{}
+	for _, api := range apis {
+		switch v := api.Service.(type) {
+		case *ethapi.EthereumAPI:
+			s.EthereumAPI = v
+		case *ethapi.BlockChainAPI:
+			s.BlockChainAPI = v
+		case *ethapi.TransactionAPI:
+			s.TransactionAPI = v
+		case *ethapi.TxPoolAPI:
+			s.TxPoolAPI = v
+		case *ethapi.DebugAPI:
+			s.DebugAPI = v
+		case *ethapi.EthereumAccountAPI:
+			s.EthereumAccountAPI = v
+		case *ethapi.PersonalAccountAPI:
+			s.PersonalAccountAPI = v
+		case *EthereumAPI:
+			s.ethEthereumAPI = v
+		case *MinerAPI:
+			s.ethMinerAPI = v
+		case *downloader.DownloaderAPI:
+			s.DownloaderAPI = v
+		case *filters.FilterAPI:
+			s.FilterAPI = v
+		case *AdminAPI:
+			s.ethAdminAPI = v
+		case *DebugAPI:
+			s.ethDebugAPI = v
+		case *ethapi.NetAPI:
+			s.NetAPI = v
+		}
 	}
+
+	return s
+
 }
 
 func (s *GrpcService) GetBlockNumber(ctx context.Context, args *protoeth.GetBlockNumberReq) (*protoeth.GetBlockNumberResp, error) {
