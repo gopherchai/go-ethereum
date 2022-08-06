eb3a600ffa3946c5ff1a999cd1edd63c9f1cea7f
gopherchai <baoyangchai@gmail.com>
2022-08-06 18:16:17 +0800

    update

--- grpc/tests/contractProj/cmd/usdt/main.go
+++ grpc/tests/contractProj/cmd/usdt/main.go
@@ -16,28 +16,12 @@ import (
 	usdt "github.com/ethereum/go-ethereum/grpc/tests/contractProj/build/contracts/usdt"
 )
 
-const account1 = `
-	{
-		"address": "f28bba82b11d654428340e910dd602193354a2b0",
-		"crypto": {
-			"cipher": "aes-128-ctr",
-			"ciphertext": "6715c81619366a65a278df8dc645ee1e37018f7f69587b2d052ff722d4ba4d1e",
-			"cipherparams": {
-				"iv": "c15f273af10280556d933c80cea6a8b9"
-			},
-			"kdf": "scrypt",
-			"kdfparams": {
-				"dklen": 32,
-				"n": 4096,
-				"p": 6,
-				"r": 8,
-				"salt": "f836fce9e561a38e83a3cee54331ee9abfac2e63babec17530e7a2f8ef3aac23"
-			},
-			"mac": "3e83e4964bf2f7622efc7fe42a4803ae44c5da7e2732b976d00276f8c5007582"
-		},
-		"id": "e18c3d2a-48c3-4bf8-849a-d61223ef0d28",
-		"version": 3
-	}`
+var (
+	account0 = `{"address":"17a8bdcc966016e68f8df27b85df29fafa0f6b43","crypto":{"cipher":"aes-128-ctr","ciphertext":"667e3a71c59dfe6e30da90efbf6e347ca5c854ed2685733f967e49e251acda99","cipherparams":{"iv":"7810570381616b4fb95ad7539928e820"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":4096,"p":6,"r":8,"salt":"5416614f984c774c08e6fd5b7ed7b2c33ee0d9ed09fe438f04effe305ab7cc94"},"mac":"886f1f3bc19608986e56f3e5293c0d301c3c2f21577e27cf65dbcc5c85c83102"},"id":"80b7af94-0427-44a6-bfb2-825194c7577b","version":3}`
+	address0 = common.HexToAddress(`17a8bdcc966016e68f8df27b85df29fafa0f6b43`)
+	address2 = common.HexToAddress(`f28bba82b11d654428340e910dd602193354a2b0`)
+	address1 = common.HexToAddress(`3e09c78573e56fda7168d86bbd0e287b11ea1f00`)
+)
 
 func main() {
 	log.SetFlags(log.Lshortfile)
@@ -46,7 +30,7 @@ func main() {
 		log.Panicln(err)
 		return
 	}
-	key, err := keystore.DecryptKey([]byte(account1), "")
+	key, err := keystore.DecryptKey([]byte(account0), "")
 	if err != nil {
 		log.Panicln(err)
 		return
@@ -77,9 +61,10 @@ func main() {
 
 	auth := bind.NewKeyedTransactor(privateKey)
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)      // in wei
-	auth.GasLimit = uint64(1000000) // in units
-	auth.GasPrice = big.NewInt(100000)
+	///auth.Value = big.NewInt(0) // in wei
+	//auth.GasLimit = uint64(1000000) // in units
+	auth.GasPrice = big.NewInt(875000000)
+
 	log.Printf("%d\n", nonce)
 
 	addr, tx, instance, err := usdt.DeployUsdt(auth, client, big.NewInt(1000000000), "usdt", "usdts", big.NewInt(3))
@@ -91,9 +76,25 @@ func main() {
 	if instance == nil {
 		panic("nil")
 	}
+	time.Sleep(time.Second * 12)
+	total, err := instance.BalanceOf(&bind.CallOpts{
+		Pending:     false,
+		From:        address0,
+		BlockNumber: big.NewInt(-1),
+	}, address0)
+	if err != nil {
+		//出现execution reverted报错，需要进一步研究
+		//追踪到core/vm/interpreter.go:141行文件中
+		log.Fatalf("%+v", err)
+	}
+	log.Println("total:", total.Int64())
+
 	tx, err = instance.Transfer(&bind.TransactOpts{
-		GasPrice: big.NewInt(10000),
-	}, common.HexToAddress("0x3e09c78573e56fda7168d86bbd0e287b11ea1f00"), big.NewInt(200))
+		From: address0,
+		//GasTipCap: big.NewInt(52),
+		GasFeeCap: big.NewInt(659086302),
+		//GasPrice: big.NewInt(100),
+	}, address1, big.NewInt(200))
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
